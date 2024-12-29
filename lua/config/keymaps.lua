e676c50fb505f43dbb8097674ed246c03806ca98
geraldQOO <g123erald456@gmail.com>
2024-12-29 20:33:13 +0800

    add undotree and telescope, but telescop doesn't ready

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- undotree
+vim.keymap.set("n", "<F5>", vim.cmd.UndotreeToggle)
