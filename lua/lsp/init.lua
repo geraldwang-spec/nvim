9bbe91f341a697389d45a1c054a4ddd7c1ebaaa7
geraldQOO <g123erald456@gmail.com>
2024-12-29 14:23:12 +0800

    first lazyvim

--- lua/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  vim.notify("lspconfig fail")
-	return
-end
-
-require("lsp.mason")
-require("lsp.handlers").setup()
-require("lsp.null-ls")
