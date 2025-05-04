e4face523008e53269260c034cf8921051a3871e
gerald <g123erald456@gmail.com>
2025-05-04 16:50:55 +0800

    add key for flutter

--- lua/util/flutter_tools.lua
+++ lua/util/flutter_tools.lua
@@ -4,4 +4,12 @@ if not status_ok then
 end
 
 flutter.setup({})
-vim.keymap.set("n", "ft", "<cmd>FlutterOutlineToggle<CR>")
+vim.keymap.set("n", "Ft", "<cmd>FlutterOutlineToggle<CR>")
+vim.keymap.set("n", "Fd", "<cmd>FlutterDevices<CR>")
+vim.keymap.set("n", "Fe", "<cmd>FlutterEmulators<CR>")
+vim.keymap.set("n", "Fl", "<cmd>FlutterLogToggle<CR>")
+vim.keymap.set("n", "Fc", "<cmd>FlutterLogClear<CR>")
+vim.keymap.set("n", "Frr", "<cmd>FlutterReload<CR>")
+vim.keymap.set("n", "FR", "<cmd>FlutterRestart<CR>")
+vim.keymap.set("n", "Fr", "<cmd>FlutterRun<CR>")
+vim.keymap.set("n", "Fq", "<cmd>FlutterQuit<CR>")
